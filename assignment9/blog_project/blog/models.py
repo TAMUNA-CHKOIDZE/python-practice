200b166da16abbf44ce0b28a991848b67d36a272
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-09-05 19:43:35 +0400

    Query the Database (Create instances and Filter posts by category)

--- assignment9/blog_project/blog/models.py
+++ assignment9/blog_project/blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+
 class Post(models.Model):
     title = models.CharField(verbose_name='სათაური', max_length=100)
     content = models.TextField(verbose_name='ტექსტი', )
     created_at = models.DateTimeField(verbose_name='შექმნის დრო', auto_now_add=True)
     category = models.CharField(verbose_name='კატეგორია', max_length=100)
+
+    def __str__(self):
+        return self.title
