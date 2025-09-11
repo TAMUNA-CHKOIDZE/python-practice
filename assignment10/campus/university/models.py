a060afb9665427f238f05c901d1750bfd9c86b1d
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-09-11 15:29:33 +0400

    create Author and Book models

--- assignment10/campus/university/models.py
+++ assignment10/campus/university/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+# 1. Author's model
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def book_count(self):
+        return self.book_set.count()
+
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+# 2. Book's model
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    # Many - to - One Relationship(ForeignKey), ერთ ავტორს აქვს ბევრი წიგნი
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name="Author")
+
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+
+    def __str__(self):
+        return self.title
