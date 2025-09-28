c11f33870405bbe775f8243e32c6cc3921f51d4b
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-09-28 20:48:34 +0400

    Implement custom user model using email as login identifier. Created CustomUser model inheriting from AbstractBaseUser and PermissionsMixin. Added CustomUserManager with create_user and create_superuser methods. Set email as USERNAME_FIELD and added full_name as required field. Updated settings.py to use AUTH_USER_MODEL = 'accounts.CustomUser'. egistered CustomUser with Django admin using a custom UserAdmin.

--- assignment15/custom_auth_project/custom_auth_project/settings.py
+++ assignment15/custom_auth_project/custom_auth_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
