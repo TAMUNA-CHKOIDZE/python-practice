ce3cf628769406bb5d9c3472ec729866cc8f8cec
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-10-03 17:54:13 +0400

    Add book_detail class-based view and register it in urls.py

--- assignment_17/book_api/books/urls.py
+++ assignment_17/book_api/books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from books.views import books_list
+from books.views import books_list, BookDetail
 
 urlpatterns = [
     path('books_list/', books_list, name='books_list'),
+    path('book_detail/<int:id>/', BookDetail.as_view(), name='book_detail')
 ]
--- assignment_17/book_api/books/views.py
+++ assignment_17/book_api/books/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from books.models import Book
 from books.serializers import BookSerializer
@@ -11,3 +12,12 @@ def books_list(request):
     books = Book.objects.filter(is_deleted=False)
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class BookDetail(APIView):
+    def get(self, request, id):
+        book = Book.objects.filter(id=id, is_deleted=False).first()
+        if book is None:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = BookSerializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
