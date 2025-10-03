ea4ff82f01e1f0f3701ce8c733b992d1c9e85ad2
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-10-03 16:56:33 +0400

    Add Book model and register it in Django admin

--- assignment_17/book_api/books/admin.py
+++ assignment_17/book_api/books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from books.models import Book
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'is_deleted')
+    search_fields = ('title', 'author')
+    list_filter = ('published_date', 'is_deleted')
--- assignment_17/book_api/books/models.py
+++ assignment_17/book_api/books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100, verbose_name='book title')
+    author = models.CharField(max_length=100, verbose_name='book author')
+    published_date = models.DateField(verbose_name='published date')
+    is_deleted = models.BooleanField(default=False, verbose_name='is deleted') # soft delete-ისთვის
+
+    class Meta:
+        ordering = ['published_date']  # სორტირება გამოქვეყნების თარიღის მიხედვით
+        verbose_name = 'book'
+        verbose_name_plural = 'books'
+        # ერთი და იგივე ავტორზე არ შეიძლება არსებობდეს ორი წიგნი, რომელსაც ერთი და იგივე სათაური აქვს, მაგრამ სხვადასხვა ავტორებს შეუძლიათ ერთნაირი სახელის წიგნი ჰქონდეთ.
+        unique_together = ('title', 'author')
+
+    def __str__(self):
+        return self.title
