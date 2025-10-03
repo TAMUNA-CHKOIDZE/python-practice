7fb6767e9c0db288c9c5c7d218f185d9f8c76c6e
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-10-03 18:12:25 +0400

    Change path

--- assignment_17/book_api/books/urls.py
+++ assignment_17/book_api/books/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from books.views import books_list, BookDetail
 
 urlpatterns = [
-    path('books_list/', books_list, name='books_list'),
-    path('book_detail/<int:id>/', BookDetail.as_view(), name='book_detail')
+    path('', books_list, name='books_list'),
+    path('<int:id>/', BookDetail.as_view(), name='book_detail')
 ]
