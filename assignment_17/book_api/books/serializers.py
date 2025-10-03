4c9d779b2936aa7090ade4544c4456472e145bb0
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-10-03 17:17:17 +0400

    Add BookSerializer.

---
+++ assignment_17/book_api/books/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+class BookSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=100)
+    published_date = serializers.DateField()
+    author = serializers.CharField(max_length=100)
+    is_deleted = serializers.BooleanField(required=False)
