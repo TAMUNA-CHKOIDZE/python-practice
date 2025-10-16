330b6c1ef6a6302fecaea0b0f46a4af5f049e76f
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-10-16 15:49:12 +0400

    Update permissions

--- assignment_20/project/articles/permissions.py
+++ assignment_20/project/articles/permissions.py
@@ -4,12 +4,6 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
         # თუ ავტორია მხოლოდ მაშინ შეუძლია რაიმე ცვლილება
-        if request.method in SAFE_METHODS:  # GET, HEAD, OPTIONS
-            return True
-        return obj.author == request.user
-        # მხოლოდ სტატიის ავტორს შეუძლია შეცვლა ან წაშლა
-        # if request.method in ['PUT', 'PATCH', 'DELETE']:
-        #     return obj.author == request.user
-        # return True  # სხვებისთვის მხოლოდ წაკითხვაა დაშვებული
-
-
+        if request.method in ['PUT', 'PATCH', 'DELETE']:
+            return obj.author == request.user
+        return True
