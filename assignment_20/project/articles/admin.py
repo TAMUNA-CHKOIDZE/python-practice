36a7f2882f8ecf3f7c3a37a4ad5cb947513e4cf7
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-10-16 13:30:13 +0400

    Created Article model and registered it in admin.py

--- assignment_20/project/articles/admin.py
+++ assignment_20/project/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from articles.models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published', 'created_at')
+    list_filter = ('published', 'created_at')
+    search_fields = ('title', 'content', 'author__email')
--- assignment_20/project/articles/models.py
+++ assignment_20/project/articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings  # ამის საშუალებით ავიღებთ AUTH_USER_MODEL-ს და არტიკლის author იქნება მომხმარებელი, რომელიც CustomUser-ით შეიქმნება და არ გამოვიყენებ ჯანგოს დეფაულტ user-ს
 
-# Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='articles', verbose_name='author')
+    title = models.CharField(max_length=255, verbose_name='title')
+    content = models.TextField(verbose_name='content')
+    published = models.BooleanField(default=False, verbose_name='published')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='updated at')
+
+    class Meta:
+        verbose_name = 'article'
+        verbose_name_plural = 'articles'
+
+    def __str__(self):
+        return self.title
